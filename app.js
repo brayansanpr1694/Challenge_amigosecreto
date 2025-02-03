92019c0397f86bbf03ac31ae8b0d8e3316f53689
brayansantamaria <brayan.santamaria.2002@gmail.com>
2025-02-03 15:09:52 -0500

    avance en el proyecto

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let cadenaAmigos = [];
+let amigos = [];
 
-function agregarAmigo(){
-    
-};
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = "";  // Limpiar el campo de entrada
+        mostrarListaAmigos()
+    }
+}
+
+function mostrarListaAmigos(){
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");  
+        li.textContent = amigos[i];  
+        lista.appendChild(li); 
+    }
+}
